db95b2bb553cd8b6f10129074f08f385be6dc39e
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-11-12 21:06:46 -0600

    refactored hero to separate component

---
+++ components/Hero.tsx
@@ -0,0 +1,67 @@
+import { Box, Button, Stack, Typography } from "@mui/material";
+
+function Hero() {
+    return (
+        <Stack
+            component="section"
+            direction="row"
+            sx={{
+                mt: 12,
+                mb: 3,
+                minHeight: '80vh',
+            }}>
+            <Box
+                sx={{
+                    margin: '0 auto',
+                    alignContent: 'center',
+                    textAlign: 'center',
+                    p: 1
+                }}
+            >
+                <Typography
+                    variant="h1"
+                    align="center"
+                    sx={{
+                        fontSize: '3rem',
+                    }}
+                >
+                    Learn More
+                </Typography>
+                <Typography
+                    variant="h2"
+                    align="center"
+                    sx={{
+                        fontSize: '3rem'
+                    }}
+                >
+                    About Earth's Moon
+                </Typography>
+                <Button
+                    href="#moon-facts"
+                    variant="contained"
+                    sx={{
+                        mt: 2,
+                        backgroundColor: '#000'
+                    }}
+                >
+                    Go to Moon Data
+                </Button>
+            </Box>
+            <Box
+                component="img"
+                alt="NASA image of Earth's Moon"
+                src="https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e001982/GSFC_20171208_Archive_e001982~orig.jpg?w=1536&h=864&fit=clip&crop=faces%2Cfocalpoint"
+                sx={{
+                    borderRadius: '5%',
+                    margin: '0 auto',
+                    width: { xs: 240, sm: 300, md: 600 },
+                    height: 'auto',
+                    objectFit: 'cover',
+                    boxShadow: 3,
+                }}
+            />
+        </Stack>
+    )
+}
+
+export default Hero;
--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,13 @@
-import { Box, Button, Stack, Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import NavBar from "../components/NavBar"
+import Hero from "../components/Hero"
 
 function App() {
 
   return (
     <>
       <NavBar />
-      <Stack
-        component="section"
-        direction="row"
-        sx={{
-          mt: 12,
-          mb: 3,
-          minHeight: '80vh',
-        }}>
-        <Box
-          sx={{
-            margin: '0 auto',
-            alignContent: 'center',
-            textAlign: 'center',
-            p: 1
-          }}
-        >
-          <Typography
-            variant="h1"
-            align="center"
-            sx={{
-              fontSize: '3rem',
-            }}
-          >
-            Learn More
-          </Typography>
-          <Typography
-            variant="h2"
-            align="center"
-            sx={{
-              fontSize: '3rem'
-            }}
-          >
-            About Earth's Moon
-          </Typography>
-          <Button
-            href="#moon-facts"
-            variant="contained"
-            sx={{
-              mt: 2,
-              backgroundColor: '#000'
-            }}
-          >
-            Go to Moon Data
-          </Button>
-        </Box>
-        <Box
-          component="img"
-          alt="NASA image of Earth's Moon"
-          src="https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e001982/GSFC_20171208_Archive_e001982~orig.jpg?w=1536&h=864&fit=clip&crop=faces%2Cfocalpoint"
-          sx={{
-            borderRadius: '5%',
-            margin: '0 auto',
-            width: { xs: 240, sm: 300, md: 600 },
-            height: 'auto',
-            objectFit: 'cover',
-            boxShadow: 3,
-          }}
-        />
-      </Stack>
+      <Hero />
       <Box component="section">
         <Typography
           id="moon-facts"
