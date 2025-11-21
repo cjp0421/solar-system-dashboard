55d3aec50f359a8c523a2b9d093d2b90a602462f
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-11-21 10:20:12 -0600

    refactored to move my components directory inside src directory to better follow industry standards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material"
-import NavBar from "../components/NavBar"
-import Hero from "../components/Hero"
+import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
 
 function App() {
 
