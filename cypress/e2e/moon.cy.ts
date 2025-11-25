db1cd56ff863181be5af36a445492b97a2250289
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-11-25 17:16:56 -0600

    attempting to make the url more flexible for running in github actions

--- cypress/e2e/moon.cy.ts
+++ cypress/e2e/moon.cy.ts
@@ -3,7 +3,7 @@ describe('Moon', () => {
         cy.intercept(
             {
                 method: 'GET',
-                url: /proxy.*id=moon.*/
+                url: /proxy/
             },
             (req) => {
                 req.on('response', (res) => {
@@ -16,13 +16,7 @@ describe('Moon', () => {
         cy.visit('/');
     });
 
-
-
     it('displays the hero section and CTA navigation', () => {
-        // cy.visit('/');
-        cy.intercept('GET', '**', (req) => {
-            console.log('🌙 CI FETCH URL:', req.url);
-        }).as('debugAll');
         cy.get('header').within(() => {
             cy.contains('h6', 'Solar System Dashboard').should('be.visible');
             cy.contains('a', 'About');
@@ -32,27 +26,27 @@ describe('Moon', () => {
         cy.get('h2').contains("About Earth's Moon").should('be.visible');
 
         cy.get('img')
-            .should('have.attr', 'src', 'https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e001982/GSFC_20171208_Archive_e001982~orig.jpg?w=1536&h=864&fit=clip&crop=faces%2Cfocalpoint')
+            .should(
+                'have.attr',
+                'src',
+                'https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e001982/GSFC_20171208_Archive_e001982~orig.jpg?w=1536&h=864&fit=clip&crop=faces%2Cfocalpoint'
+            );
 
-        cy.get('a').contains('Go to Moon Data').click();
+        cy.contains('Go to Moon Data').click();
         cy.get("#moon-facts").scrollIntoView();
 
-        cy.get('h3').contains("Facts About Earth's Moon").should('be.visible');
+        cy.contains("Facts About Earth's Moon").should('be.visible');
         cy.url().should('include', '#moon-facts');
     });
     // MD - 001 - Loading State
     it('shows skeleton loaders while data is fetching', () => {
-        // cy.visit('/');
-        cy.intercept('GET', '**', (req) => {
-            console.log('🌙 CI FETCH URL:', req.url);
-        }).as('debugAll');
         cy.contains('Go to Moon Data').click();
 
         cy.get('#moon-facts').within(() => {
             cy.get('[role="progressbar"]').should('exist');
-        })
+        });
 
-        cy.wait('@getMoon')
+        cy.wait('@getMoon');
 
         cy.get('#moon-facts').within(() => {
             cy.contains('Mass').should('be.visible');
@@ -64,26 +58,18 @@ describe('Moon', () => {
             cy.contains('Escape Velocity').should('be.visible');
 
             cy.get('[role="progressbar"]').should('not.exist');
-        })
-    })
+        });
+    });
+
     // MD - 002 - Success State 
     it('displays Moon data after successful load', () => {
-        // cy.intercept('GET', '**/proxy?id=moon', {
-        //     fixture: 'moon.json',
-        // }).as('getMoon');
-
-
-        // cy.visit('/');
-        cy.intercept('GET', '**', (req) => {
-            console.log('🌙 CI FETCH URL:', req.url);
-        }).as('debugAll');
         cy.contains('Go to Moon Data').click();
 
         cy.wait('@getMoon');
 
         cy.get('#moon-facts').within(() => {
             cy.contains("Facts About Earth's Moon").should('be.visible');
-        })
+        });
 
         cy.get('#moon-facts').within(() => {
             cy.contains('Mass').should('be.visible');
@@ -98,6 +84,5 @@ describe('Moon', () => {
         });
 
         cy.contains('🚨ERROR🚨').should('not.exist');
-    })
-
-})
+    });
+});
