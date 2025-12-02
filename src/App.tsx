2e0ec3e33218d2eb4830ad12bb70af9a4c9d6f8d
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-12-01 18:20:34 -0600

    added styling to ensure footer remains at the bottom of the screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,28 @@ import Layout from "./routes/Layout";
 import LandingPage from "./routes/LandingPage";
 import MoonPage from "./routes/MoonPage";
 import NotFoundPage from "./routes/NotFoundPage";
+import { Box } from "@mui/material";
+import './index.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/solar-system-dashboard/" element={<LandingPage />} />
-          <Route path="/solar-system-dashboard/moon" element={<MoonPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/solar-system-dashboard/" element={<LandingPage />} />
+            <Route path="/solar-system-dashboard/moon" element={<MoonPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Box>
   )
 }
 
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/navBar/NavBar";
 import Footer from "../components/Footer";
+import { Box } from "@mui/material";
 
 export default function Layout() {
     return (
         <>
             <NavBar />
-            <main>
+            <Box component="main" sx={{ flex: 1 }}>
                 <Outlet />
-            </main>
+            </Box>
             <Footer />
         </>
     )
