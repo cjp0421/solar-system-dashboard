2c71afc2ebd1564f7b3d2e3e5ec52f22625039fa
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-12-03 12:37:28 -0600

    added app-wide font - Space Grotesk and cssbaseline to eliminate styling differences between browsers

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { theme } from "./theme";
 
 const client = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
