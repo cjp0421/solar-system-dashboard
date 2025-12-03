bdb83117ff7c66b88e5f3808075564888ad6b592
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-12-03 09:25:38 -0600

    added mui icons to footer and cleaned up footer styles for mobile and desktop

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,8 @@
 import { Box, Link, Typography } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LanguageIcon from '@mui/icons-material/Language';
+
 
 function Footer() {
     return (
@@ -12,7 +16,13 @@ function Footer() {
                 py: 1,
             }}
         >
-            <Typography variant="body2" sx={{ visibility: "hidden" }}>
+            <Typography
+                variant="body2"
+                sx={{
+                    visibility: "hidden",
+                    fontSize: '.5rem',
+                }}
+            >
                 © Carol Pedersen 2025
             </Typography>
 
@@ -20,15 +30,75 @@ function Footer() {
                 sx={{
                     display: "flex",
                     justifyContent: "center",
+                    alignContent: 'center',
                     gap: 4,
                 }}
             >
-                <Link href="https://github.com/cjp0421/solar-system-dashboard">GitHub</Link>
-                <Link href="https://www.linkedin.com/in/carol-joy-pedersen/">LinkedIn</Link>
-                <Link href="https://cjp0421.github.io/my-portfolio/">Portfolio</Link>
+                <Link
+                    href="https://github.com/cjp0421/solar-system-dashboard"
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 0.6,
+                        color: "inherit",
+                        textDecoration: "none"
+                    }}
+                >
+                    <GitHubIcon fontSize="small" />
+                    <Box
+                        sx={{
+                            display: { xs: "none", md: "inline" }
+                        }}
+                    >
+                        GitHub
+                    </Box>
+                </Link>
+                <Link
+                    href="https://www.linkedin.com/in/carol-joy-pedersen/"
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 0.6,
+                        color: "inherit",
+                        textDecoration: "none"
+                    }}
+                >
+                    <LinkedInIcon fontSize="small" />
+                    <Box
+                        sx={{
+                            display: { xs: "none", md: "inline" }
+                        }}
+                    >
+                        LinkedIn
+                    </Box>
+                </Link>
+                <Link
+                    href="https://cjp0421.github.io/my-portfolio/"
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: 0.6,
+                        color: "inherit",
+                        textDecoration: "none"
+                    }}
+                >
+                    <LanguageIcon fontSize="small" />
+                    <Box
+                        sx={{
+                            display: { xs: "none", md: "inline" }
+                        }}
+                    >
+                        Portfolio
+                    </Box>
+                </Link>
             </Box>
 
-            <Typography variant="body2">
+            <Typography
+                variant="body2"
+                sx={{
+                    fontSize: '.5rem',
+                }}
+            >
                 © Carol Pedersen 2025
             </Typography>
         </Box>
